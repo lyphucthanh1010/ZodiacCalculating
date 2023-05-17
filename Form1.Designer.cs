1b7cb640997d61a9aa2ec7b342e1f46e056fd389
Lý Phúc Thành <114668542+lyphucthanh1010@users.noreply.github.com>
2023-05-17 09:01:23 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace ZodiacCalculating
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Nhập năm sinh:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 17);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Nhập tháng sinh:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Nhập ngày sinh:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(151, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 30;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 29;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 28;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(623, 49);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 77);
+            this.button4.TabIndex = 27;
+            this.button4.Text = "12 Cung hoàng đạo";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 17);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Cung hoàng đạo của bạn là:";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(35, 380);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(216, 112);
+            this.richTextBox3.TabIndex = 25;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(295, 147);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(384, 345);
+            this.richTextBox2.TabIndex = 24;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(37, 150);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(214, 170);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(513, 49);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 77);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(404, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 77);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Xoá";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(295, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 77);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Kiểm tra kết quả";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 541);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Zodiac Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZodiacCalculating
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private bool NamNhuan(int nam)
+        {
+            if (nam % 400 == 0 || nam % 4 == 0 && nam % 100 != 0)
+                return true;
+            else
+                return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                int NgayLonNhat;
+                int ngay = Int32.Parse(textBox1.Text);
+                int thang = Int32.Parse(textBox2.Text);
+                int nam = Int32.Parse(textBox3.Text);
+                if (nam < 1930)
+                {
+                    MessageBox.Show("Năm không hợp lệ", "Thông báo", MessageBoxButtons.OK);
+                }
+
+                if (thang < 1 || thang > 12)
+                {
+                    MessageBox.Show("Tháng không hợp lệ", "Thông báo", MessageBoxButtons.OK);
+                }
+                bool Kiemtra = NamNhuan(nam);
+                if (thang == 2 && Kiemtra == true)
+                {
+                    NgayLonNhat = 29;
+                }
+                else
+                {
+                    switch (thang)
+                    {
+                        case 4:
+                        case 6:
+                        case 9:
+                        case 11:
+                            NgayLonNhat = 30;
+                            break;
+                        case 2:
+                            NgayLonNhat = 28;
+                            break;
+                        default:
+                            NgayLonNhat = 31;
+                            break;
+                    }
+                }
+                if (ngay < 1 || ngay > NgayLonNhat)
+                {
+                    MessageBox.Show("Ngày không hợp lệ vì vượt quá số ngày của tháng", "Thông báo", MessageBoxButtons.OK);
+                }
+                DateTime date = new DateTime(nam, thang, ngay);
+                int NgayTrongTuan = (int)date.DayOfWeek + 1;
+                if (NgayTrongTuan == 1)
+                    richTextBox1.Text += "Đây là ngày đầu tiên trong tuần (Chủ nhật)";
+                else
+                    richTextBox1.Text += "Đây là ngày thứ " + NgayTrongTuan.ToString() + " " + "trong tuần" + " " + "(Thứ " + NgayTrongTuan + ")" + "\n";
+
+                switch (thang)
+                {
+                    case 1:
+                        if (ngay >= 20)
+                        {
+                            richTextBox3.Text = "Bảo Bình";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Ma Kết";
+                        }
+                        break;
+
+                    case 2:
+                        if (ngay >= 19)
+                        {
+                            richTextBox3.Text = "Song Ngư";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Bảo Bình";
+                        }
+                        break;
+
+                    case 3:
+                        if (ngay >= 21)
+                        {
+                            richTextBox3.Text = "Bạch Dương";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Song Ngư";
+                        }
+                        break;
+
+                    case 4:
+                        if (ngay < 20)
+                        {
+                            richTextBox3.Text = "Bạch Dương";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Kim Ngưu";
+                        }
+                        break;
+
+                    case 5:
+                        if (ngay >= 21)
+                        {
+                            richTextBox3.Text = "Song Tử";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Kim Ngưu";
+                        }
+                        break;
+
+                    case 6:
+                        if (ngay < 21)
+                        {
+                            richTextBox3.Text = "Song Tử";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Cự Giải";
+                        }
+                        break;
+
+                    case 7:
+                        if (ngay >= 23)
+                        {
+                            richTextBox3.Text = "Sư Tử";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Cự Giải";
+                        }
+                        break;
+
+                    case 8:
+                        if (ngay < 23)
+                        {
+                            richTextBox3.Text = "Sư Tử";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Xử Nữ";
+                        }
+                        break;
+
+                    case 9:
+                        if (ngay >= 23)
+                        {
+                            richTextBox3.Text = "Thiên Bình";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Xử Nữ";
+                        }
+                        break;
+
+                    case 10:
+                        if (ngay < 23)
+                        {
+                            richTextBox3.Text = "Thiên Bình";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Thần Nông";
+                        }
+                        break;
+
+                    case 11:
+                        if (ngay >= 22)
+                        {
+                            richTextBox3.Text = "Nhân Mã";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Thần Nông";
+                        }
+                        break;
+
+                    case 12:
+                        if (ngay < 22)
+                        {
+                            richTextBox3.Text = "Nhân Mã";
+                        }
+                        else
+                        {
+                            richTextBox3.Text = "Ma Kết";
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "21/03 - 20/04 : Cung Bạch Dương \n" +
+              "21/04 - 21/05 : Cung Kim Ngưu \n" +
+              "22/05 - 21/06 : Cung Song Tử \n" +
+              "22/06 - 22/07 : Cung Cự Giải \n" +
+              "23/07 - 22/08 : Cung Sư Tử \n" +
+              "23/08 - 23/09 : Cung Xử Nữ \n" +
+              "24/09 - 23/10 : Cung Thiên Bình \n" +
+              "24/10 - 22/11 : Cung Thần Nông \n" +
+              "23/11 - 21/12 : Cung Nhân Mã \n" +
+              "22/12 - 20/01 : Cung Ma Kết \n" +
+              "21/01 - 19/02 : Cung Bảo Bình \n" +
+              "20/02 - 20/03 : Cung Song Ngư \n";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            richTextBox1.Text = " ";
+            richTextBox2.Text = " ";
+            richTextBox3.Text = " ";
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
